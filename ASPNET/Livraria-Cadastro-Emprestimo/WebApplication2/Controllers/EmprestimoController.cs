c0a7d284f9ccb9457ceaa80137cf19dd0fa68e9b
Capelari <alandc@hotmail.com.br>
2024-02-06 21:46:17 -0300

    Envio de projeto de ASP.NET

---
+++ ASPNET/Livraria-Cadastro-Emprestimo/WebApplication2/Controllers/EmprestimoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        List<EmprestimoModel> emprestimos = new List<EmprestimoModel> { };
+
+        Data.ContextoDB contexto = new Data.ContextoDB();
+
+        public IActionResult Index()
+        {
+            var emprestimos = contexto.Emprestimos.ToList();
+
+            return View(emprestimos);
+        }
+
+        [HttpPost]
+        public IActionResult Index(EmprestimoModel emprestimo)
+        {
+
+            contexto.Emprestimos.Add(emprestimo);
+
+            contexto.SaveChanges();
+
+            var emprestimosDb = contexto.Emprestimos.ToList();
+
+            return View(emprestimosDb);
+        }
+        public IActionResult Cadastra()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastra(EmprestimoModel emprestimo)
+        {
+            emprestimos.Add(emprestimo);
+            return View(emprestimo);
+        }
+    }
+}
---
+++ ASPNET/Livraria-Cadastro-Emprestimo/WebApplication2/Controllers/LivroController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class LivroController : Controller
+    {
+        List<LivroModel> livros = new List<LivroModel> { };
+
+        Data.ContextoDB contexto = new Data.ContextoDB();
+
+        public IActionResult Index()
+        {
+            var livros = contexto.Livros.ToList();
+
+            return View(livros);
+        }
+
+        [HttpPost]
+        public IActionResult Index(LivroModel livroCadastro)
+        {
+
+            contexto.Livros.Add(livroCadastro);
+
+            contexto.SaveChanges();
+
+            var livrosDb = contexto.Livros.ToList();
+
+            return View(livrosDb);
+        }
+
+        public IActionResult Cadastra()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastra(LivroModel livroCadastro)
+        {
+            livros.Add(livroCadastro);
+            return View(livroCadastro);
+        }
+    }
+}
