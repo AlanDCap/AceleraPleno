73f1f2db367abafe4af3b470ae38d30bd3aec07c
Capelari <alandc@hotmail.com.br>
2024-03-20 12:13:54 -0300

    Author: Alan Capelari Date: 20/03/24 Reason: Envio de projeto de autenticação de apis

--- API/ProjetoAPI/obj/Debug/net8.0/ProjetoAPI.AssemblyInfo.cs
+++ API/ProjetoAPI/obj/Debug/net8.0/ProjetoAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b8c26ef047abf6420a775c65cfc1cbfd3b3385")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
